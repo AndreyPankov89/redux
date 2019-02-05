cc19245c90a5c157b922dd344355bf80f1b499d6
hjicvi <apan89@mail.ru>
2019-02-05 16:20:54 +0500

    lesson 12 adv+base

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {createStore} from 'redux';
-//import JsonService from './service/jsonService'
+import JsonService from './service/jsonService'
 
 const reducer = (state = 0, action) => {
     switch (action.type){
@@ -9,9 +9,9 @@ const reducer = (state = 0, action) => {
         case 'DEC':{
             return state - 1;
         }
-  //      case 'UPL':{
-    //        return action.value;
-      //  }
+        case 'UPL':{
+            return action.value;
+        }
         case 'RST':{
             return 0;
         }
@@ -23,26 +23,72 @@ const reducer = (state = 0, action) => {
 
 }
 
+
 // const getRandomObject = async () =>{
-//     const service = new JsonService();
-//     const numbers = await service.getResource();
-//     console.log(numbers);
-//     return numbers[Math.floor(Math.random()*3)].const;
+//     // const service = new JsonService();
+//     // const res = await service.getResource()
+//     //     .then(res =>{
+//     //         return +res[Math.floor(Math.random()*3)].const;
+//     //     })
+    
+//     // console.log(res);
+//     // return res;
+
+//     const rnd = Math.floor(Math.random() * 10);
+//     let value =  await fetch('http://localhost:3004/numbers')
+//         .then(res => res.json())
+//         .then(res => {
+//             if (rnd <= 3) {
+//                 return +res[0].const;
+//             } else if (rnd > 3 && rnd <= 6) {
+//                 return +res[1].const;
+//             } else {
+//                 return +res[2].const;
+//             }
+//         });
+//     console.log(value);
+// //    const value = Math.floor(Math.random()*10)
+//     const action = {type: 'UPL', value};
+//     console.log(action)
+//     return action;
+//     return await value
+
 // }
 
 const inc = () => ({type:'INC'});
 const dec = () => ({type:'DEC'});
 const rst = () => ({type:'RST'});
-// const upl = async () =>{
-//     const value = await getRandomObject();
-//     return {type: 'UPL', value}
-// };
+const upl = (value) =>{
+    return {type:'UPL', value}
+};
 
 const store = createStore(reducer);
 document.getElementById('plus').addEventListener('click', ()=>{store.dispatch(inc())});
 document.getElementById('minus').addEventListener('click', ()=>{store.dispatch(dec())});
 document.getElementById('reset').addEventListener('click', ()=>{store.dispatch(rst())});
-// document.getElementById('upload').addEventListener('click', ()=>{console.log('123');store.dispatch(upl())});
+document.getElementById('upload').addEventListener('click', async ()=>{
+    const rnd = Math.floor(Math.random() * 10);
+
+    let value =  await fetch('./db.json')
+        .then(res => res.json())
+        .then(res => {
+            if (rnd <= 3) {
+                return +res.numbers[0].const;
+            } else if (rnd > 3 && rnd <= 6) {
+                return +res.numbers[1].const;
+            } else {
+                return +res.numbers[2].const;
+            }
+        });
+    await store.dispatch(upl(value));
+
+});
+document.getElementById('download').addEventListener('click', async ()=>{
+    const jsonService = new JsonService();
+    const id = await jsonService.getLastId()+1;
+    const data = {saved: store.getState(), id: id};
+    jsonService.postData(data);
+});
 
 store.subscribe(() => {
      document.getElementById('counter').innerHTML = store.getState();
--- src/service/jsonService.js
+++ src/service/jsonService.js
@@ -1,23 +1,22 @@
 export default class JsonService{
 
 
-    async getResource(){
-        const res = await fetch('http://localhost:3000/numbers');
+    async getLastId(){
+        const res = await fetch('http://localhost:3004/numbers');
 
         if (!res.ok){
             throw new Error ('cannot fetch db.json');
         }
 
         console.log('get res')
-        return await res.json();
-    }
-
-    getJson(){
-        return this.getResource();
+        return await res.json().then((res) =>{
+            return res[res.length-1].id
+        });
     }
 
     async postData(data){
-        const post = await fetch('http://localhost:3000/numbers',{
+        const post = await fetch('http://localhost:3004/numbers',{
+            method:"POST",
             headers: {'content-type': 'application/json'},
             body: JSON.stringify(data)
         })
